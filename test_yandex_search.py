f54030043fa3acb91b523ebf73abb50541ba4971
Sabir Aliyev <sabir.aliyev.itdep@gmail.com>
2021-04-06 13:29:38 +0400

    Simple test of https://ya.ru with using of second tab implemented.

--- test_yandex_search.py
+++ test_yandex_search.py
@@ -1,10 +1,29 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def test_yandex_search():
-    driver = WebDriver(executable_path='/usr/local/bin/geckodriver')
+    driver = WebDriver(executable_path='/usr/local/bin/chromedriver')
     driver.set_page_load_timeout(30)
     driver.get("https://ya.ru")
-    driver.maximize_window()
-    print(None)
-    ...
+    # driver.maximize_window()
+    search_input = driver.find_element_by_xpath('//input[@id="text"]')
+    search_button = driver.find_element_by_xpath('//div[@class="search2__button"]//button[@type="submit"]')
+    search_input.send_keys('market.yandex.ru')
+    search_button.click()
+
+    def check_result_count(driver):
+        inner_search_results = driver.find_elements_by_xpath('//li[@class="serp-item"]')
+        return len(inner_search_results) >= 10
+
+    # check_result_count is sending as link.
+    WebDriverWait(driver, 5, 0.5).until(check_result_count, 'Number of elements is less then 10')
+
+    search_results = driver.find_elements_by_xpath('//li[@class="serp-item"]')
+    link = search_results[0].find_element_by_xpath('.//h2/a')
+    link.click()
+
+    driver.switch_to.window(driver.window_handles[1])
+
+    assert driver.title == 'Яндекс.Маркет — выбор и покупка товаров из проверенных интернет-магазинов'
+
